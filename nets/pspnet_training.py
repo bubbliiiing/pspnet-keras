3d6f8a35b8cf1e33d059e2aa6986470e355498b0
Bubbliiiing <3323290568@qq.com>
2022-03-24 16:08:50 +0800

    update a lot

--- nets/pspnet_training.py
+++ nets/pspnet_training.py
@@ -1,3 +1,6 @@
+import math
+from functools import partial
+
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
@@ -7,7 +10,11 @@ def dice_loss_with_CE(cls_weights, beta=1, smooth = 1e-5):
     cls_weights = np.reshape(cls_weights, [1, 1, 1, -1])
     def _dice_loss_with_CE(y_true, y_pred):
         y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-
+        #------------------------------------------------------------------------#
+        #   在VOC数据集中，部分标签中物体存在白边，这些白边是训练时需要去忽略的。
+        #   白边的像素点值为255，在数据载入时，默认将白边值调整成num_classes + 1
+        #   在one_hot处理后，我们只需要取前num_classes序号的内容来进行训练即可
+        #------------------------------------------------------------------------#
         CE_loss = - y_true[...,:-1] * K.log(y_pred) * cls_weights
         CE_loss = K.mean(K.sum(CE_loss, axis = -1))
 
@@ -26,7 +33,11 @@ def CE(cls_weights):
     cls_weights = np.reshape(cls_weights, [1, 1, 1, -1])
     def _CE(y_true, y_pred):
         y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-
+        #------------------------------------------------------------------------#
+        #   在VOC数据集中，部分标签中物体存在白边，这些白边是训练时需要去忽略的。
+        #   白边的像素点值为255，在数据载入时，默认将白边值调整成num_classes + 1
+        #   在one_hot处理后，我们只需要取前num_classes序号的内容来进行训练即可
+        #------------------------------------------------------------------------#
         CE_loss = - y_true[...,:-1] * K.log(y_pred) * cls_weights
         CE_loss = K.mean(K.sum(CE_loss, axis = -1))
         # dice_loss = tf.Print(CE_loss, [CE_loss])
@@ -37,7 +48,11 @@ def dice_loss_with_Focal_Loss(cls_weights, beta=1, smooth = 1e-5, alpha=0.5, gam
     cls_weights = np.reshape(cls_weights, [1, 1, 1, -1])
     def _dice_loss_with_Focal_Loss(y_true, y_pred):
         y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-
+        #------------------------------------------------------------------------#
+        #   在VOC数据集中，部分标签中物体存在白边，这些白边是训练时需要去忽略的。
+        #   白边的像素点值为255，在数据载入时，默认将白边值调整成num_classes + 1
+        #   在one_hot处理后，我们只需要取前num_classes序号的内容来进行训练即可
+        #------------------------------------------------------------------------#
         logpt = - y_true[...,:-1] * K.log(y_pred) * cls_weights
         logpt = - K.sum(logpt, axis = -1)
 
@@ -62,7 +77,11 @@ def Focal_Loss(cls_weights, alpha=0.5, gamma=2):
     cls_weights = np.reshape(cls_weights, [1, 1, 1, -1])
     def _Focal_Loss(y_true, y_pred):
         y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-
+        #------------------------------------------------------------------------#
+        #   在VOC数据集中，部分标签中物体存在白边，这些白边是训练时需要去忽略的。
+        #   白边的像素点值为255，在数据载入时，默认将白边值调整成num_classes + 1
+        #   在one_hot处理后，我们只需要取前num_classes序号的内容来进行训练即可
+        #------------------------------------------------------------------------#
         logpt = - y_true[...,:-1] * K.log(y_pred) * cls_weights
         logpt = - K.sum(logpt, axis = -1)
 
@@ -73,3 +92,42 @@ def Focal_Loss(cls_weights, alpha=0.5, gamma=2):
         CE_loss = K.mean(CE_loss)
         return CE_loss
     return _Focal_Loss
+
+def get_lr_scheduler(lr_decay_type, lr, min_lr, total_iters, warmup_iters_ratio = 0.1, warmup_lr_ratio = 0.1, no_aug_iter_ratio = 0.3, step_num = 10):
+    def yolox_warm_cos_lr(lr, min_lr, total_iters, warmup_total_iters, warmup_lr_start, no_aug_iter, iters):
+        if iters <= warmup_total_iters:
+            # lr = (lr - warmup_lr_start) * iters / float(warmup_total_iters) + warmup_lr_start
+            lr = (lr - warmup_lr_start) * pow(iters / float(warmup_total_iters), 2
+            ) + warmup_lr_start
+        elif iters >= total_iters - no_aug_iter:
+            lr = min_lr
+        else:
+            lr = min_lr + 0.5 * (lr - min_lr) * (
+                1.0
+                + math.cos(
+                    math.pi
+                    * (iters - warmup_total_iters)
+                    / (total_iters - warmup_total_iters - no_aug_iter)
+                )
+            )
+        return lr
+
+    def step_lr(lr, decay_rate, step_size, iters):
+        if step_size < 1:
+            raise ValueError("step_size must above 1.")
+        n       = iters // step_size
+        out_lr  = lr * decay_rate ** n
+        return out_lr
+
+    if lr_decay_type == "cos":
+        warmup_total_iters  = min(max(warmup_iters_ratio * total_iters, 1), 3)
+        warmup_lr_start     = max(warmup_lr_ratio * lr, 1e-6)
+        no_aug_iter         = min(max(no_aug_iter_ratio * total_iters, 1), 15)
+        func = partial(yolox_warm_cos_lr ,lr, min_lr, total_iters, warmup_total_iters, warmup_lr_start, no_aug_iter)
+    else:
+        decay_rate  = (min_lr / lr) ** (1 / (step_num - 1))
+        step_size   = total_iters / step_num
+        func = partial(step_lr, lr, decay_rate, step_size)
+
+    return func
+
